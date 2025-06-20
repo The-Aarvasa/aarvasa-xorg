713cc081ee04e6313b1557a4e7e14c8b1f5ae837
Mohammed-Azhan <azhanpalli@gmail.com>
2025-06-21 00:08:54 +0530

    merged

--- arvasa---frontend/src/pages/PropertyDetails.jsx
+++ arvasa---frontend/src/pages/PropertyDetails.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import About from '../components/PropertyBrief/About'
 import {Explore} from '../components/PropertyBrief/Explore'
-import {Amenities} from '../components/PropertyBrief/Amenities'
+// import Amenities from '../components/PropertyBrief/Amenities'
 import Slider from '../components/About/Slider'
 import Sale from '../components/PropertyBrief/Sale'
 import Photos from '../components/PropertyBrief/Photos'
@@ -12,12 +12,12 @@ const PropertyDetails = () => {
   return (
     <>
         
-        <div className='bg-orange-50 pt-[10px] mb-[20px] mx-5'>
+        <div className='bg-orange-50 pt-[10px] mb-[20px] mx-2'>
             <Photos />
             <Sale />
             <About />
             <Explore />
-            <Amenities />
+            {/* <Amenities /> */}
             
         </div>
         <Slider />
