4d0e86b711bd9bfd99d516babf36f9742e2ac66b
Mohammed-Azhan <azhanpalli@gmail.com>
2025-07-26 15:23:37 +0530

    my changes

--- arvasa---frontend/src/App.jsx
+++ arvasa---frontend/src/App.jsx
@@ -68,7 +68,7 @@ function AppLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <main className="flex-grow">
-  
+
         {!hideNavAndFooter && <Navbar />}
 
         <Routes>
@@ -78,26 +78,26 @@ function AppLayout() {
           <Route path="/otp" element={<Otp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/agents" element={<Agents />} />
-            <Route path="/agents/:id" element={<AgentDetails />} />
-            <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/agents" element={<Agents />} />
+          <Route path="/agents/:id" element={<AgentDetails />} />
+          <Route path="/services" element={<Services />} />
 
-            <Route path="/listings" element={<Listing />}>
-              <Route index element={<PropertyMain />} />
-              <Route path="ratestrends" element={
-                <>
-                  <ShowTrends />
-                  <Graphs />
-                </>
-              } />
-            </Route>
+          <Route path="/listings" element={<Listing />}>
+            <Route index element={<PropertyMain />} />
+            <Route path="ratestrends" element={
+              <>
+                <ShowTrends />
+                <Graphs />
+              </>
+            } />
+          </Route>
 
-            <Route path="/listings/propertydetails/:id" element={<PropertyDetails />} />
+          <Route path="/listings/propertydetails/:id" element={<PropertyDetails />} />
 
           {/* Protected Routes */}
           <Route element={<PrivateRoute />}>
-            
+
             <Route path="/news" element={<News />} />
 
             <Route path="/profile" element={<Profile />}>
@@ -130,7 +130,7 @@ function AppLayout() {
 
 
           {/* Fallback */}
-            <Route path="/subscription" element={<Subscription />} />
+          <Route path="/subscription" element={<Subscription />} />
 
         </Routes>
       </main>
@@ -145,7 +145,7 @@ function AppLayout() {
 function App() {
   return (
     <Router>
-      <AuthProvider> 
+      <AuthProvider>
         <FilterProvider>
           <AppLayout />
           <ToastContainer
@@ -158,8 +158,8 @@ function App() {
             draggable
             pauseOnHover
             theme="colored"
-            className="z-[99999999] mt-16 w-full p-8" 
-  toastClassName="rounded-md shadow-lg"
+            className="z-[99999999] mt-16 w-full p-8"
+            toastClassName="rounded-md shadow-lg"
           />
         </FilterProvider>
       </AuthProvider>
